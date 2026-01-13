936716140973bd8a72c88c7adcfbc9e505f78bc7
Nathan <natanribeirobarbosa4161@gmail.com>
2026-01-13 11:49:12 -0300

    adicionando edição de vitrine

--- show.js
+++ show.js
@@ -48,33 +48,16 @@ function carregarProdutos() {
 }
 
 function carregarTodosProdutos() {
-  const lista = document.getElementById("products")
+    const lista = document.getElementById("lista")
 
-  const roupaRef = doc(db, "roupas", "lBi3lHOQHCQJvbtytYzx")
+  onSnapshot(collection(db, "users"), snapshot => {
+    lista.innerHTML = ""
 
-  onSnapshot(roupaRef, (docSnap) => {
-    if (!docSnap.exists()) return
-
-    const dados = docSnap.data()
-    const itens = dados.itens // 👈 campo array do Firestore
-
-    let content = ""
-
-    itens.forEach(item => {
-      content += `
-        <div class="product">
-          <img src="${item.linkF}" height="160px" alt="">
-          <div>
-            <span class="">${item.nome}</span>
-            <button class="buy" onclick="window.location.href='${item.link}'">
-              Acessar link
-            </button>
-          </div>
-        </div>
-      `
+    snapshot.forEach(doc => {
+      const li = document.createElement("li")
+      li.textContent = doc.data().nome
+      lista.appendChild(li)
     })
-
-    lista.innerHTML = content
   })
 }
 carregarProdutos()
