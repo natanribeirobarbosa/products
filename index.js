3502dca5b175b52fc59f4406daed9a3475ddd1e6
Nathan <natanribeirobarbosa4161@gmail.com>
2026-01-21 13:03:49 -0300

    adicionando options

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const botoes = document.querySelectorAll(".options button");
 
 botoes.forEach(botao => {
     botao.addEventListener("click", () => {
+        document.getElementById("products").innerHTML= `<p>Carregando...</p>`
         botoes.forEach(b => b.classList.remove("checked"));
         botao.classList.add("checked");
 
-        
+
 
     });
 });
